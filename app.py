bf2b0d4f36d9a117ead0f931c3286d9e4e72c1d7
nandini-singh11 <147043647+nandini-singh11@users.noreply.github.com>
2025-07-20 23:31:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import speech_recognition as sr
 import tempfile, os
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates')
 CORS(app)
 
 @app.route('/')
